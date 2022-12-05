e3f3fea32eecb58e7ea454b7ad48a9e76e7d0cbd
JC <luffy33820@gmail.com>
2022-12-05 22:54:56 +0900

    added image handling

--- p2p.py
+++ p2p.py
@@ -1,23 +1,41 @@
 import socket, threading, sys
 
+BUFFER_SIZE = 256
+
 class P2PServer:        # Class for the P2P server
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
     clients_list = []       # List of clients
 
     def __init__(self):     # Constructor
-        self.sock.bind(('0.0.0.0', 8000))       # Bind to port 8000
+        self.sock.bind(('0.0.0.0', 8001))       # Bind to port 8000
         self.sock.listen(1)         # Listen for connections
 
     def handler(self, c, addr):        # Handler for client connections
         while True:          # Loops forever
-            data = c.recv(1024)          # Receive data from client
-            for connection in self.clients_list:         # Send data to all clients in the list
-                connection.send(data)        # except the one sending the data
-            if not data:        # If no data is received:
+            ftype = c.recv(BUFFER_SIZE).decode()          # Receive data from client
+            ftype = int(ftype)        # Convert data to int
+            
+            if ftype == 4:
                 print(str(addr[0]) + ':' + str(addr[1]), "disconnected")      # Print that the client disconnected
                 self.clients_list.remove(c)         # Remove the client from the list
                 c.close()       # Close the connection
                 break       # Exit the loop
+            
+            for connection in self.clients_list:         # Send data to all clients in the list
+                if ftype == 2:   # if the data received is an image file
+                    d = connection.recv(BUFFER_SIZE).decode()
+                    img = open(d, 'rb')
+                   
+                    while True:
+                        data = img.readline(BUFFER_SIZE)
+                        if not data:
+                           break
+                        connection.send(data)
+                    img.close()
+                    print("Image sent to", str(addr[0]) + ':' + str(addr[1]))
+                   
+                if ftype == 3:
+                    connection.send(data.encode())        # except the one sending the data
 
     def run(self):      # Runs the server
         while True:     # Loop forever
@@ -33,24 +51,52 @@ class P2PClient:        # Class for the P2P client
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        # Create a socket object
     
     def __init__(self, address):     # Constructor
-        self.sock.connect((address, 8000))      # Connect to the server
+        self.sock.connect((address, 8001))      # Connect to the server
         while True:
-            file = input("What file do you wanna send?\n >1 CSV/JSON file\n >2 IMG file\n >3 Simple string message\n")     # Ask the user what file they want to send
+            # print list of addresses connected to the server
+            #print(P2PServer.clients_list)
+            #dest = input ("Enter the destination IP address: ")     # Get the destination IP address
+            file = input("What file do you wanna send?\n >1 CSV/JSON file\n >2 IMG file\n >3 Simple string message\n >4 Disconnect\n")     # Ask the user what file they want to send
             
-            if file == "1" or file == "2":      # If the user wants to send a file:
+            if file == "1" or file == "2":      # If the user wants to send a csv, json or img file:
                 file_name = input("Enter the file name: ")      # Ask for the file name
-            
-            # STILL TO COMPLETE
-            
+                
+                if file == "2":     # image:
+                    self.sock.send(file.encode())
+                    print("Sending file saved as: " + file_name + "\n")
+                    self.send_file(file_name)
+                    file_name = 'docs/' + file_name
+                    with open(file_name, 'wb') as f:
+                        while True:
+                            data = self.sock.recv(BUFFER_SIZE)
+                            if not data:
+                                break
+                            f.write(data)
+                    f.close()
+                     
             if file == "3":     # If the user wants to send a simple string message:
+                self.sock.send(file.encode())
                 message = input("Enter the message: ") 
                 self.send_msg(message)        # Send the message
+                data = self.sock.recv(BUFFER_SIZE)     # Receive data from the server
+                print(str(data, 'utf-8'))       # Print the data received from the server
             
-            data = self.sock.recv(1024)     # Receive data from the server
-            print(str(data, 'utf-8'))       # Print the data received from the server
+            if file == "4":
+                self.sock.send(file.encode())
+                print("Disconnecting...")
+                break
+    
     
     def send_msg(self, msg):     # Function for sending messages
         self.sock.send(bytes(msg, 'utf-8'))       # Send the message to the server
+
+    def send_file(self, file_name):     # Function for sending files
+        self.sock.send(file_name.encode())     # Send the file name to the server
+        print("File sent successfully\n")
+
+            
+
+
 
 
 
